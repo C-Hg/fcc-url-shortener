361f3b49da5f2f236328991acb4d88e8789ff960
Camille Hagenbourger <camille.hagenbourger@gmail.com>
2018-11-20 18:58:44 +0100

    escape callback hell

--- controllers/shortURL.controller.js
+++ controllers/shortURL.controller.js
@@ -1,49 +1,38 @@
 const ShortURL = require('../models/shortURL.model');
 
-exports.send_short_url = function (req, res) {
-    res.json({ original_url: req.body.url, short_url: "surprise", total_url: ShortURL.collection.countDocuments() });
+exports.evaluate_original_url = async function (req, res) {
+    let input_url = await ShortURL.findOne({ 'original_URL': req.body.url }, handleURLsearch);
+
+    //if no match found, count documents to assign proper number to the new URL
+    if (!input_url) {
+        let count = await ShortURL.countDocuments(handleCount);
+        create_and_display_new_url(req, res, count);
+    }
+
+    // retrieves url already shortened
+    else {
+        res.json({ 'original_url': req.body.url, 'short_url': input_url.short_URL });
+    }
 };
 
-
-exports.search_original_url = function (req, res) {
-    ShortURL.findOne({ 'original_URL': req.body.url }, function (err, url_details) {
-        if (err) return next(err);
-        //create the new entry if not already in the database (url_detail returns NULL if no match)
-
-        if (!url_details) {
-            let totalURLs = 0;
-
-            const retrieveNumberOfDocuments = async () => {
-                await ShortURL.countDocuments(function (err, count){
-                    if (err) return err;
-                    let url = new ShortURL({
-                        'original_URL': req.body.url,
-                        'short_URL': count + 1
-                    });
-                    url.save(function (err) {
-                        if (err) {return (err); }
-                        res.json({ 'original_URL': req.body.url, 'short_URL': url.short_URL });
-                        console.log("New short URL added to database");
-                    })               
-                });
-            }
-            retrieveNumberOfDocuments();        
-        }
-        // retrieves url already shortened
-        else {
-            res.json({ 'original_url': req.body.url, 'short_url': url_details.short_URL });
-        }
-    })
+async function handleURLsearch(err, url_details) {
+    if (err) return console.error(err);
+    return url_details;
 };
 
-exports.create_new_url = function (req, res) {
-    let url = new ShortURL({
-        'original_url': req.body.url,
-        'short_url': 1
+function handleCount(err, count) {
+    if (err) return err;
+    return count;
+}
+
+create_and_display_new_url = function (req, res, count) {
+    let newUrl = new ShortURL({
+        'original_URL': req.body.url,
+        'short_URL': count + 1
     });
-    url.save(function (err) {
+    newUrl.save(function (err) {
         if (err) { return handleError(err); }
-        res.json({ 'original_url': req.body.url, 'short_url': url.short_URL });
+        res.json({ 'original_url': req.body.url, 'short_url': newUrl.short_URL });
         console.log("New short URL added to database");
     })
 };
